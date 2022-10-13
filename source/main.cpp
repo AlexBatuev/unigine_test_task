7babd78265a3989c44ebfde4efbf6e40e5c804df
Alexander Batuev <batuev.prizrak@gmail.com>
2022-10-13 17:56:09 +0900

    Добавил класс для менеджмента точек сплайнов

--- source/main.cpp
+++ source/main.cpp
@@ -1,6 +1,6 @@
 #include "framework/engine.h"
 #include "framework/utils.h"
-#include "../catmull_rom.h"
+#include "catmull_rom.h"
 
 using namespace std;
 using namespace glm;
@@ -12,7 +12,58 @@ using namespace glm;
 * z - backward
 */
 
+// ============================================================
 
+class points_holder
+{
+	vector<vec3> spline_points;
+
+	static float get_distance(const vec3& v1, const vec3& v2) {
+		const auto dx = v1.x - v2.x;
+		const auto dy = v1.y - v2.y;
+		const auto dz = v1.z - v2.z;
+		return sqrt(dx * dx + dy * dy + dz * dz);
+	}
+
+public:
+	vector<vec3> create_spline_points(const float path[], const size_t path_count, float min_distance = 0.1f)
+	{
+		vector<vec3> path_v;
+		for (size_t i = 0; i < path_count; i += 3)
+		{
+			path_v.emplace_back(vec3 (path[i], path[i + 1], path[i + 2]));
+		}
+
+		const vector<vec3> all_spline_points = catmull_rom::calculate(path_v, 0.5f, 500);
+
+		spline_points.clear();
+		for(auto point : all_spline_points)
+		{
+			if (!spline_points.empty() && get_distance(point, spline_points[spline_points.size() - 1]) < min_distance)
+			{
+				continue;
+			}
+			spline_points.emplace_back(point);
+		}
+
+		return spline_points;
+	}
+
+	///	Get a path point in the range from 0 to 1
+	vec3 get_point_at(const float alpha) const
+	{
+		const auto index = clamp<size_t>(alpha * spline_points.size(), 0, spline_points.size() - 1);
+		return spline_points[index];
+	}
+};
+
+inline float get_angle(vec3 v1, vec3 v2)
+{
+	// todo: PI
+	return atan2(v2.z - v1.z, v2.x - v1.x) * 180.f / 3.14f;
+}
+
+/// class time map
 
 // ============================================================
 
@@ -64,15 +115,19 @@ int main()
 
 	// ========================================================
 
-	vector<vec3> path_v;
-	for (size_t i = 0; i < std::size(path); i += 3)
-	{
-		vec3 v(path[i], path[i + 1], path[i + 2]);
-		path_v.emplace_back(v);
-	}
+	auto holder = points_holder();
+	auto spline_points = holder.create_spline_points(path, std::size(path), 0.01f);
+	// init
 
-	vector<vec3> spline_points = catmull_rom::calculate(path_v, 0.5f, 10);
-	size_t spline_points_limit = spline_points.size();
+	// test cube
+	auto cube_mesh = createCube();
+	Object* cube = engine->createObject(&cube_mesh);
+	cube->setColor(0.5f, 0.2f, 0.2f);
+	cube->setPosition(0, 0.5f, 0);
+	cube->setRotation(-90.0f, 0.0f, 0.0f);
+	cube->setScale(1.f);
+
+	// path drawing
 	vector<Object*> points2;
 	for (auto& spline_point : spline_points)
 	{
@@ -84,19 +139,6 @@ int main()
 	}
 	LineDrawer path_drawer2(spline_points, true);
 
-	// test cube
-	auto cube_mesh = createCube();
-	Object* cube = engine->createObject(&cube_mesh);
-	cube->setColor(0.5f, 0.2f, 0.2f);
-	cube->setPosition(0, 0.5f, 0);
-	cube->setRotation(-90.0f, 0.0f, 0.0f);
-	cube->setScale(1.f);
-
-	/// class points holder
-	///	get point between 0 to 1
-	
-	/// class time map
-
 	/// new cube rotation
 	// get current position
 	// get new position by deltatime
@@ -105,7 +147,7 @@ int main()
 	// set new rotation
 
 	auto path_t = 0.f;
-	auto speed = 0.1f;
+	auto speed = 0.05f;
 
 	// main loop
 	while (!engine->isDone())
@@ -116,10 +158,13 @@ int main()
 		{
 			path_t -= 1;
 		}
-		auto index2 = clamp<size_t>(path_t * spline_points_limit, 0, spline_points_limit - 1);
-		auto current_point = spline_points[index2];
-		cube->setPosition(current_point);
-		cout << path_t << " : " << index2 << " : " << current_point.x << " " << current_point.y << " " << current_point.z << endl;
+		auto old_point = cube->getPosition();
+		auto new_point = holder.get_point_at(path_t);
+		auto angle = get_angle(old_point, new_point);
+		cout << "angle: " << angle << endl;
+		cube->setPosition(new_point);
+		//cube->setRotation(angle);
+		//cout << path_t << " : " << index2 << " : " << current_point.x << " " << current_point.y << " " << current_point.z << endl;
 
 		engine->update();
 		engine->render();
